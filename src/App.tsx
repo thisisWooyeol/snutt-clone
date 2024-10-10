c3e07c0a51ae9152b9c2b86e7d335b2917b63afc
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-10-10 16:53:17 +0900

    feat: connected login api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
 import './reset.css';
 import './tailwind.css';
 
+import { useState } from 'react';
+
 import { Landing } from './pages/Landing';
+import { Login, postLogin } from './pages/Login';
 
 export const App = () => {
-  return <Landing />;
+  const [token, setToken] = useState<string | null>();
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const id = formData.get('id');
+    const pw = formData.get('pw');
+    console.info(id, pw);
+
+    postLogin({ id: id, pw: pw })
+      .then((data) => {
+        console.info(data);
+        setToken(data.token);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <>
+      <Login handler={loginHandler} />
+      <h1>Token: {token}</h1>
+    </>
+  );
 };
