8461b9898042297fcb5f012f4c7da80f6adfc7b6
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-10-16 17:20:58 +0900

    🎨 Use env context for api base url

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -17,18 +17,15 @@ export type AuthApi = {
   getUser: (token: string) => Promise<UserInfo>;
 };
 
-export const getAuthApi = (): AuthApi => ({
+export const getAuthApi = (API_BASE_URL: string): AuthApi => ({
   signInWithPassword: async (req: LoginRequest): Promise<LoginResponse> => {
-    const response = await fetch(
-      `${import.meta.env.VITE_API_BASE_URL}/v1/auth/login_local`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ id: req.id, password: req.pw }),
+    const response = await fetch(`${API_BASE_URL}/v1/auth/login_local`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    );
+      body: JSON.stringify({ id: req.id, password: req.pw }),
+    });
 
     if (!response.ok) {
       throw new Error('Login failed');
@@ -37,14 +34,11 @@ export const getAuthApi = (): AuthApi => ({
     return response.json() as Promise<LoginResponse>;
   },
   getUser: async (token: string): Promise<UserInfo> => {
-    const response = await fetch(
-      `${import.meta.env.VITE_API_BASE_URL}/v1/users/me`,
-      {
-        headers: {
-          'x-access-token': token,
-        },
+    const response = await fetch(`${API_BASE_URL}/v1/users/me`, {
+      headers: {
+        'x-access-token': token,
       },
-    );
+    });
 
     if (!response.ok) {
       throw new Error('Failed to fetch user info');
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,22 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
+import { EnvContext } from './context/EnvContext';
 
 const root = document.getElementById('root');
 
 if (root === null) throw new Error('Root element not found');
 
+const ENV = {
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
+};
+
 createRoot(root).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <EnvContext.Provider value={ENV}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </EnvContext.Provider>
   </StrictMode>,
 );
