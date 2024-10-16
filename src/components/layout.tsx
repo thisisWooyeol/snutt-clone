efc590df8217351cbe8f92623440ac5dc96e1a36
Wooyeol Lee <97590283+thisisWooyeol@users.noreply.github.com>
2024-10-17 00:24:41 +0900

    💄 Remove border (#10)
    과제 스펙 준수를 위해 윤곽 제거

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,7 @@ type LayoutProps = {
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="flex h-svh w-screen items-center justify-center">
-      <div className="flex h-[49rem] w-[23.4375rem] flex-col justify-center rounded-lg border-2 border-zinc-200 bg-white">
+      <div className="flex h-[49rem] w-[23.4375rem] flex-col justify-center bg-white">
         {children}
       </div>
     </div>
