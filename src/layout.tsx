3e0d89fe312af22d06afd526494d1815c9cacf25
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-10-24 01:00:23 +0900

    🚚 Move layout from components to root (src/)
    Considering nested layout, nextjs compatability

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,10 +1,6 @@
 import { type ReactNode } from 'react';
 
-type LayoutProps = {
-  children: ReactNode;
-};
-
-export const Layout = ({ children }: LayoutProps) => {
+export const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex h-svh w-screen items-center justify-center">
       <div className="flex h-[49rem] w-[23.4375rem] flex-col justify-center bg-white">
