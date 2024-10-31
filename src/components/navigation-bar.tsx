ba67836b0b22bdb73d0b1d1d1836e7e9cbd96f8d
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-10-31 22:31:00 +0900

    ✨ Created navigation bar

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -10,9 +10,6 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 
 export const NavigationBar = () => {
-  // TODO: SNUTT 클론코딩 (2-2) - 네비게이션 바 구현하기
-  // 네비게이션 바는 시간표, 마이페이지 버튼이 있습니다.
-  // 바텀 네비바에서 제일 오른쪽에 있는 버튼을 클릭하면 마이페이지로 이동해야 합니다.
   const location = useLocation();
   const navigate = useNavigate();
   return (
