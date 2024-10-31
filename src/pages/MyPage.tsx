efc56b913e41f7738637caedd723879c111ce937
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-10-31 22:22:56 +0900

    ✅ Marked tasks 3, 6 as completed

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -15,16 +15,6 @@ export const MyPage = () => {
   const { toRoot, toMyPageAccount } = useRoutes();
   const userInfo = useLoaderData() as UserInfo;
 
-  // TODO: SNUTT 클론코딩 (2-3) - 마이페이지 구현하기
-  // 실제 snutt 앱과 유사한 디자인으로 마이페이지 만들기
-
-  // TODO: SNUTT 클론코딩 (2-6) - 닉네임 변경 기능 구현하기
-  // 내 계정 페이지는 /mypage/account 경로로
-  // 닉네임 변경 페이지는 /mypage/account/change-nickname 경로로 구현해 주세요.
-  // 닉네임 변경 API는 PATCH /v1/users/me 사용하시면 됩니다.
-  // 닉네임 변경이 완료되면 내 계정 페이지로 이동해야 하며, 이동 시 변경된 닉네임이 내 계정 페이지에 반영되어 있어야 합니다.
-  // SNUTT 앱이 그렇듯, 상단 바에 뒤로가기 버튼이 있어야 하며 뒤로가기 시 이전 페이지로 이동할 수 있어야 합니다.
-
   return (
     <div className="flex h-full flex-col bg-zinc-50">
       <PageHeader>
