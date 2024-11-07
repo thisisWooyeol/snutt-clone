87b8ff3d4dccff35f35a1a90a0af1152c080804b
Wooyeol Lee <97590283+thisisWooyeol@users.noreply.github.com>
2024-11-07 23:59:22 +0900

    ♻️ Refactor/change grid row cols (#26)

--- src/layout.tsx
+++ src/layout.tsx
@@ -3,7 +3,7 @@ import { type ReactNode } from 'react';
 export const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex h-svh w-screen items-center justify-center">
-      <div className="flex h-[49rem] w-[23.4375rem] flex-col justify-center bg-white">
+      <div className="flex h-full max-h-[49rem] w-[23.4375rem] flex-col justify-center bg-white">
         {children}
       </div>
     </div>
