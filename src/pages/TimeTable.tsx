6dee7f88925ba77d4f4031f7d78b25a069dcfa64
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-11-08 12:09:04 +0900

    🎨 Use constant variables and use filter istead of if

--- src/pages/TimeTable.tsx
+++ src/pages/TimeTable.tsx
@@ -6,26 +6,26 @@ import { NavigationBar } from '@/components/navigation-bar';
 import { PageHeader } from '@/components/page-header';
 import { cn } from '@/lib/utils';
 
+// Constants
+const DAYS_OF_WEEK = ['월', '화', '수', '목', '금'] as const;
+const START_OF_DAY = 9;
+const END_OF_DAY = 22;
+const NUM_HOURS = END_OF_DAY - START_OF_DAY + 1;
+const HOURS = Array.from({ length: NUM_HOURS }, (_, i) => START_OF_DAY + i);
+const COLORS = [
+  'bg-red-500',
+  'bg-orange-500',
+  'bg-yellow-400',
+  'bg-lime-400',
+  'bg-green-500',
+  'bg-teal-400',
+  'bg-blue-500',
+  'bg-indigo-500',
+  'bg-purple-500',
+];
+
 export const TimeTablePage = () => {
   const recentTimeTable = useLoaderData() as TimeTable;
-
-  const daysOfWeek = ['월', '화', '수', '목', '금'];
-  const startOfDay = 9;
-  const endOfDay = 22;
-  const numHours = endOfDay - startOfDay + 1;
-  const hours = Array.from({ length: numHours }, (_, i) => startOfDay + i);
-
-  const colors = [
-    'bg-red-500',
-    'bg-orange-500',
-    'bg-yellow-400',
-    'bg-lime-400',
-    'bg-green-500',
-    'bg-teal-400',
-    'bg-blue-500',
-    'bg-indigo-500',
-    'bg-purple-500',
-  ];
 
   return (
     <div className="flex h-full flex-col">
@@ -54,7 +54,7 @@ export const TimeTablePage = () => {
       {/* Days of the Week Header */}
       <div className="grid h-6 grid-cols-[5%_19%_19%_19%_19%_19%] divide-x border-y">
         <div /> {/* Placeholder for time column */}
-        {daysOfWeek.map((day, index) => (
+        {DAYS_OF_WEEK.map((day, index) => (
           <div
             key={index}
             className="flex items-center justify-center text-xs text-muted-foreground"
@@ -64,34 +64,36 @@ export const TimeTablePage = () => {
         ))}
       </div>
       {/* Timetable Grid */}
-      <div className="grid flex-grow grid-cols-[5%_19%_19%_19%_19%_19%] divide-x border-b">
+      <div className="grid flex-grow grid-cols-[5%_19%_19%_19%_19%_19%] divide-x">
         {/* Time Column */}
-        <div className="flex flex-col divide-y">
-          {hours.map((hour) => (
+        <div className="flex flex-col">
+          {HOURS.map((hour) => (
             <div
               key={hour}
-              className="flex flex-grow justify-center text-xs text-muted-foreground"
+              className="flex flex-grow justify-center border-b text-xs text-muted-foreground"
             >
               {hour}
             </div>
           ))}
         </div>
         {/* Schedule Cells by Day */}
-        {daysOfWeek.map((_, dayIndex) => (
-          <div key={dayIndex} className="relative flex flex-col divide-y">
-            {hours.map((_, hourIndex) => (
-              <div key={hourIndex} className="relative flex-grow">
+        {DAYS_OF_WEEK.map((_, dayIndex) => (
+          <div key={dayIndex} className="relative flex flex-col">
+            {HOURS.map((_, hourIndex) => (
+              <div key={hourIndex} className="relative flex-grow border-b">
                 {/* Horizontal Divider */}
                 <div className="absolute inset-x-0 top-1/2 border-t border-muted" />
               </div>
             ))}
             {/* Add Lectures for the Day */}
             {recentTimeTable.lecture_list.map((lecture, lectureIndex) =>
-              lecture.class_time_json.map((classTime, classTimeIndex) => {
-                if (classTime.day === dayIndex) {
+              lecture.class_time_json
+                .filter((classTime) => classTime.day === dayIndex)
+                .map((classTime, classTimeIndex) => {
                   const startHourPosition =
-                    classTime.startMinute / 60 - startOfDay;
-                  const endHourPosition = classTime.endMinute / 60 - startOfDay;
+                    classTime.startMinute / 60 - START_OF_DAY;
+                  const endHourPosition =
+                    classTime.endMinute / 60 - START_OF_DAY;
                   const duration = endHourPosition - startHourPosition;
 
                   return (
@@ -99,19 +101,18 @@ export const TimeTablePage = () => {
                       key={`${lectureIndex}-${classTimeIndex}`}
                       className={cn(
                         'absolute inset-x-0 flex flex-col justify-center p-2 text-center text-xs font-bold text-white',
-                        colors[lecture.colorIndex],
+                        COLORS[lecture.colorIndex % COLORS.length],
                       )}
                       style={{
-                        top: `${(startHourPosition * 100) / numHours}%`,
-                        height: `${(duration * 100) / numHours}%`,
+                        top: `${(startHourPosition * 100) / NUM_HOURS}%`,
+                        height: `${(duration * 100) / NUM_HOURS}%`,
                       }}
                     >
                       <p>{lecture.course_title}</p>
                       <p>{classTime.place}</p>
                     </div>
                   );
-                }
-              }),
+                }),
             )}
           </div>
         ))}
