0726696d79843e61de7b583548040468c278cf1e
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-11-10 00:17:02 +0900

    💡 과제3 todo 작성

---
+++ src/pages/LectureList.tsx
@@ -0,0 +1,14 @@
+export const LectureListPage = () => {
+  return (
+    // TODO: 강의 목록 페이지 구현
+    // `/timetables/:id/lectures` route로 들어오는 페이지
+    // 시간표 화면 오른쪽 위 : drawer icon을 통해 진입
+    // 지도 정보는 비워주세요
+    // 아이템을 클릭하면 1번에서 만든 강의 상세화면으로 이동
+    // get timetable api: https://snutt-api-dev.wafflestudio.com/webjars/swagger-ui/index.html#/default/getTimetable
+
+    <div>
+      <h1>Lecture List Page</h1>
+    </div>
+  );
+};
