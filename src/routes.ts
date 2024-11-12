87f2594e4f047cdde80544df2f6d84f7795b513e
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-11-13 01:39:17 +0900

    ✨ Create route function to make dynamic route path

--- src/routes.ts
+++ src/routes.ts
@@ -7,4 +7,12 @@ export const ROUTES = {
   TIMETABLE_LECTURE: '/timetables/:timetableId/lectures/:lectureId',
   TIMETABLE_LECTURELIST: '/timetables/:timetableId/lectures',
   TIMETABLE_NEW: '/timetables/:timetableId/new',
+
+  // Function to generate a path with parameters
+  getTimetableLecturePath: (timetableId: string, lectureId: string) =>
+    `/timetables/${timetableId}/lectures/${lectureId}`,
+  getTimetableLectureListPath: (timetableId: string) =>
+    `/timetables/${timetableId}/lectures`,
+  getTimetableNewPath: (timetableId: string) =>
+    `/timetables/${timetableId}/new`,
 };
