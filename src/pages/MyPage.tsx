e0d7fa784baf03e064c9243a1a6623e2b82564bd
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-11-14 00:48:46 +0900

    ♻️ Modularize mypage block rows

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,9 +1,9 @@
 import { ChevronRight, User } from 'lucide-react';
-import { NavLink, useLoaderData } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 
 import { type UserInfo } from '@/api/types';
 import { DestructiveDialog } from '@/components/destructive-dialog';
-import { MyPageButton } from '@/components/mypage-button';
+import { MyPageItem, MyPageSection } from '@/components/mypage-detail';
 import { NavigationBar } from '@/components/navigation-bar';
 import { PageHeader } from '@/components/page-header';
 import { Avatar } from '@/components/ui/avatar';
@@ -14,26 +14,28 @@ export const MyPage = () => {
   const userInfo = useLoaderData() as UserInfo;
   useSearchParamsAlert();
 
+  const CHEVRON_RIGHT_MUTED = (
+    <ChevronRight className="text-muted-foreground" />
+  );
+
   return (
     <div className="flex h-full flex-col">
       <PageHeader>
         <div className="flex items-center gap-2 p-4">
-          <img
-            src="/tab/tab_mypage_off.svg"
-            alt="mypage"
-            style={{ width: '24px', height: '24px' }}
-          />
+          <img src="/tab/tab_mypage_off.svg" alt="mypage" className="size-6" />
           <div className="font-bold">더보기</div>
         </div>
       </PageHeader>
 
       <main className="flex-1 bg-muted">
-        <div className="my-2 bg-background">
-          <MyPageButton asChild>
-            <NavLink to={ROUTES.MYPAGE_ACCOUNT}>
+        <MyPageSection>
+          <MyPageItem
+            asChild
+            to={ROUTES.MYPAGE_ACCOUNT}
+            leftContent={
               <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <div className="flex h-6 w-6 items-center justify-center rounded-full bg-muted">
+                <Avatar className="size-6">
+                  <div className="flex size-6 items-center justify-center rounded-full bg-muted">
                     <User />
                   </div>
                 </Avatar>
@@ -44,92 +46,93 @@ export const MyPage = () => {
                   </span>
                 </div>
               </div>
+            }
+            rightContent={
               <div className="flex items-center gap-2 text-muted-foreground">
                 <span>
                   {`${userInfo.nickname.nickname}#${userInfo.nickname.tag}`}
                 </span>
                 <ChevronRight />
               </div>
-            </NavLink>
-          </MyPageButton>
-        </div>
+            }
+          />
+        </MyPageSection>
 
-        <div className="my-2 bg-background">
-          <MyPageButton>
-            <span>화면 모드</span>
-            <div className="flex items-center gap-2 text-muted-foreground">
-              <span>자동</span>
-              <ChevronRight />
-            </div>
-          </MyPageButton>
+        <MyPageSection>
+          <MyPageItem
+            leftContent={<span>화면 모드</span>}
+            rightContent={
+              <div className="flex items-center gap-2 text-muted-foreground">
+                <span>자동</span>
+                {CHEVRON_RIGHT_MUTED}
+              </div>
+            }
+          />
+          <MyPageItem
+            leftContent={<span>시간표 설정</span>}
+            rightContent={CHEVRON_RIGHT_MUTED}
+          />
+          <MyPageItem
+            leftContent={<span>시간표 테마</span>}
+            rightContent={CHEVRON_RIGHT_MUTED}
+          />
+        </MyPageSection>
 
-          <MyPageButton>
-            <span>시간표 설정</span>
-            <ChevronRight className="text-muted-foreground" />
-          </MyPageButton>
+        <MyPageSection>
+          <MyPageItem
+            leftContent={<span>빈자리 알림</span>}
+            rightContent={CHEVRON_RIGHT_MUTED}
+          />
+        </MyPageSection>
 
-          <MyPageButton>
-            <span>시간표 테마</span>
-            <ChevronRight className="text-muted-foreground" />
-          </MyPageButton>
-        </div>
-
-        <div className="my-2 bg-background">
-          <MyPageButton>
-            <span>빈자리 알림</span>
-            <ChevronRight className="text-muted-foreground" />
-          </MyPageButton>
-        </div>
-
-        <div className="my-2 bg-background">
-          <div className="flex h-12 w-full items-center justify-between px-4 py-2">
-            <span className="text-sm">버전 정보</span>
-            <span className="text-sm text-muted-foreground">3.8.2</span>
+        <MyPageSection>
+          <div className="flex h-12 w-full items-center justify-between px-4 py-2 text-sm">
+            <span>버전 정보</span>
+            <span className="text-muted-foreground">3.8.2</span>
           </div>
 
-          <MyPageButton>
-            <span>개발자 정보</span>
-            <ChevronRight className="text-muted-foreground" />
-          </MyPageButton>
-        </div>
+          <MyPageItem
+            leftContent={<span>개발자 정보</span>}
+            rightContent={CHEVRON_RIGHT_MUTED}
+          />
+        </MyPageSection>
 
-        <div className="my-2 bg-background">
-          <MyPageButton>
-            <span>개발자 괴롭히기</span>
-            <ChevronRight className="text-muted-foreground" />
-          </MyPageButton>
-        </div>
+        <MyPageSection>
+          <MyPageItem
+            leftContent={<span>개발자 괴롭히기</span>}
+            rightContent={CHEVRON_RIGHT_MUTED}
+          />
+        </MyPageSection>
 
-        <div className="my-2 bg-background">
-          <MyPageButton>
-            <span>오픈소스 라이선스</span>
-            <ChevronRight className="text-muted-foreground" />
-          </MyPageButton>
+        <MyPageSection>
+          <MyPageItem
+            leftContent={<span>오픈소스 라이선스</span>}
+            rightContent={CHEVRON_RIGHT_MUTED}
+          />
+          <MyPageItem
+            leftContent={<span>서비스 약관</span>}
+            rightContent={CHEVRON_RIGHT_MUTED}
+          />
+          <MyPageItem
+            leftContent={<span>개인정보처리방침</span>}
+            rightContent={CHEVRON_RIGHT_MUTED}
+          />
+        </MyPageSection>
 
-          <MyPageButton>
-            <span>서비스 약관</span>
-            <ChevronRight className="text-muted-foreground" />
-          </MyPageButton>
-
-          <MyPageButton>
-            <span>개인정보처리방침</span>
-            <ChevronRight className="text-muted-foreground" />
-          </MyPageButton>
-        </div>
-
-        <div className="my-2 bg-background">
+        <MyPageSection>
           <DestructiveDialog
             trigger={
-              <MyPageButton className="text-destructive hover:text-destructive">
-                <span>로그아웃</span>
-                <ChevronRight />
-              </MyPageButton>
+              <MyPageItem
+                leftContent={<span>로그아웃</span>}
+                rightContent={<ChevronRight />}
+                className="text-destructive hover:text-destructive"
+              />
             }
             title="로그아웃"
             description="로그아웃 하시겠습니까?"
             action="로그아웃"
           />
-        </div>
+        </MyPageSection>
       </main>
 
       <NavigationBar />
