686ece912d0f478766f35278b0b326a9b9704042
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-11-13 13:09:40 +0900

    ✨ Implement delete table service

--- src/services/tableService.ts
+++ src/services/tableService.ts
@@ -1,17 +1,57 @@
 import { type TableApi } from '@/api/tableApi';
 import type {
+  DeleteTimetableLectureResult,
   GetTimetableByIdResult,
   GetTimetableListResult,
   GetTimetableRecentResult,
 } from '@/services/types';
 
 export type TableService = {
+  deleteTimetableLecture: (
+    timetableId: string,
+    lectureId: string,
+  ) => Promise<DeleteTimetableLectureResult>;
+  getTimetableById: (id: string) => Promise<GetTimetableByIdResult>;
   getTimetableList: () => Promise<GetTimetableListResult>;
   getTimetableRecent: () => Promise<GetTimetableRecentResult>;
-  getTimetableById: (id: string) => Promise<GetTimetableByIdResult>;
 };
 
 export const getTableService = (tableApi: TableApi): TableService => ({
+  deleteTimetableLecture: async (
+    timetableId: string,
+    lectureId: string,
+  ): Promise<DeleteTimetableLectureResult> => {
+    try {
+      const token = localStorage.getItem('token');
+      if (token === null) throw new Error('Token not found');
+
+      const data = await tableApi.deleteTimetableLecture({
+        token,
+        timetableId,
+        lectureId,
+      });
+      return { data, error: null };
+    } catch (error: unknown) {
+      return {
+        data: null,
+        error: error instanceof Error ? error.message : 'Unknown error',
+      };
+    }
+  },
+  getTimetableById: async (id: string): Promise<GetTimetableByIdResult> => {
+    try {
+      const token = localStorage.getItem('token');
+      if (token === null) throw new Error('Token not found');
+
+      const data = await tableApi.getTimetableById({ token, id });
+      return { data, error: null };
+    } catch (error: unknown) {
+      return {
+        data: null,
+        error: error instanceof Error ? error.message : 'Unknown error',
+      };
+    }
+  },
   getTimetableList: async (): Promise<GetTimetableListResult> => {
     try {
       const token = localStorage.getItem('token');
@@ -32,20 +72,6 @@ export const getTableService = (tableApi: TableApi): TableService => ({
       if (token === null) throw new Error('Token not found');
 
       const data = await tableApi.getTimetableRecent({ token });
-      return { data, error: null };
-    } catch (error: unknown) {
-      return {
-        data: null,
-        error: error instanceof Error ? error.message : 'Unknown error',
-      };
-    }
-  },
-  getTimetableById: async (id: string): Promise<GetTimetableByIdResult> => {
-    try {
-      const token = localStorage.getItem('token');
-      if (token === null) throw new Error('Token not found');
-
-      const data = await tableApi.getTimetableById({ token, id });
       return { data, error: null };
     } catch (error: unknown) {
       return {
