7bbf34c03e42a5cdd887ef2611a969611d0716b9
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-11-19 02:56:56 +0900

    💄 Make lecture list to have consistent ui style

--- src/pages/TimetableLectureList.tsx
+++ src/pages/TimetableLectureList.tsx
@@ -1,52 +1,80 @@
+import { ChevronRight } from 'lucide-react';
 import { NavLink, useLoaderData } from 'react-router-dom';
 
 import type { TimetableDetailed } from '@/api/types';
+import { PageHeader } from '@/components/page-header';
 import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
 import { ROUTES } from '@/routes';
 
 import { DAYS_OF_WEEK } from './Timetable';
 
 export const TimetableLectureList = () => {
-  const timetable = useLoaderData() as TimetableDetailed;
+  const timetableDetail = useLoaderData() as TimetableDetailed;
 
   return (
-    <div className="p-4">
-      <div className="mb-4 flex items-center gap-1">
-        <Button asChild variant="ghost" size="icon" className="size-6">
-          <NavLink to={ROUTES.ROOT}>
-            <img src="/icons/chevron-left.svg" alt="back" />
+    <div className="flex h-full flex-col">
+      <PageHeader className="border-b border-muted">
+        <div className="flex items-center gap-1 p-4">
+          <Button asChild variant="ghost" size="icon" className="size-6">
+            <NavLink to={ROUTES.ROOT}>
+              <img src="/icons/chevron-left.svg" alt="back" />
+            </NavLink>
+          </Button>
+          <h1 className="font-bold">강의 목록</h1>
+        </div>
+      </PageHeader>
+
+      <div className="flex-1">
+        {timetableDetail.lecture_list.map((lecture) => (
+          <NavLink
+            key={lecture._id}
+            to={ROUTES.getTimetableLecturePath(
+              timetableDetail._id,
+              lecture._id,
+            )}
+            className={'hover:opacity-80'}
+          >
+            <div className="flex h-28 flex-col justify-center gap-2 p-6">
+              <div className="flex justify-between">
+                <div className="text-md font-semibold">
+                  {lecture.course_title}
+                </div>
+                <div className="text-xs">
+                  {lecture.instructor} / {lecture.credit}학점
+                </div>
+              </div>
+              <div className="flex flex-col gap-1">
+                <div className="text-xs">
+                  {lecture.department}, {lecture.academic_year}
+                </div>
+                <div className="flex gap-x-2">
+                  {lecture.class_time_json.map((classTime, index) => (
+                    <span key={index} className="text-xs">
+                      {DAYS_OF_WEEK[classTime.day]}({classTime.start_time}~
+                      {classTime.end_time})
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="px-4">
+              <Separator decorative={true} className="bg-muted" />
+            </div>
           </NavLink>
-        </Button>
-        <h1 className="font-bold">강의 목록</h1>
+        ))}
+        <div className="w-full">
+          <NavLink to={ROUTES.getTimetableNewPath(timetableDetail._id)}>
+            <Button
+              variant="ghost"
+              className="h-12 w-full justify-between rounded-none p-6 font-normal"
+            >
+              <span>강의 추가하기</span>
+              <ChevronRight />
+            </Button>
+          </NavLink>
+        </div>
       </div>
-      {timetable.lecture_list.map((lecture) => (
-        <NavLink
-          key={lecture._id}
-          to={ROUTES.getTimetableLecturePath(timetable._id, lecture._id)}
-          className={'hover:opacity-80'}
-        >
-          <div className="mb-4 rounded-lg border bg-gray-50 p-4 shadow">
-            <h2 className="text-xl font-semibold">{lecture.course_title}</h2>
-            <div className="mb-2 flex justify-between text-gray-600">
-              <span className="text-sm">
-                {lecture.department}, {lecture.academic_year}
-              </span>
-              <span className="text-sm">
-                {lecture.instructor} / {lecture.credit}학점
-              </span>
-            </div>
-            <div>
-              {lecture.class_time_json.map((classTime, index) => (
-                <span key={index} className="mr-2">
-                  {DAYS_OF_WEEK[classTime.day]}({classTime.start_time}~
-                  {classTime.end_time})
-                </span>
-              ))}
-            </div>
-            <div className="text-sm text-gray-800">{}</div>
-          </div>
-        </NavLink>
-      ))}
     </div>
   );
 };
